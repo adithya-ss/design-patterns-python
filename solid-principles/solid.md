61bbc4599ceb93d690b32479efa4132d2c051f63
Adithya Seebi <adithya.seebi@epicor.com>
2023-07-23 20:44:24 +0530

    SOLID design principles - Single Responsibility Principle

---
+++ solid-principles/solid.md
@@ -0,0 +1,6 @@
+# Single Responsibility Principle
+- Also called as 'Seperation of Concerns'
+- A class should have a primary responsibility and should not have additional responsibilities
+
+Antipatterns - Opposite of a particular pattern.
+Ex: God object: Everything in the same class
---
+++ solid-principles/srp_soc.py
@@ -0,0 +1,50 @@
+class Journal:
+    """
+    The primary responsibility of this Journal class is to either add or remove an entry
+    SRP or SOC should make sure that the primary responsibility is not altered/changed; or that any other responsbility is added.
+    """
+    def __init__(self):
+        self.entries = list()
+        self.count = 0
+    
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'Entry Number {self.count}: {text}')
+    
+    def remove_entry(self, position):
+        del self.entries[position]
+    
+    def __str__(self):
+        return '\n'.join(self.entries)
+    
+    # def save_entry(self, filename):
+    #     """
+    #     The addition of this new functionality would break the SRP/SOC.
+    #     Secondary responsibility would be to save the journal entry.
+    #     To match with the SOLID principles, this has to be kept outside of the class.
+    #     """
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+    
+class PersitanceManager:
+    """
+    The responsibility of this class is to saving the object to a file.
+    """
+    @staticmethod
+    def save_entry_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+
+
+if __name__ == "__main__":
+    journal_obj = Journal()
+    journal_obj.add_entry('Nothing is impossible.')
+    journal_obj.add_entry('Patience is virtue.')
+    print(f'Below are the journal entries: \n{journal_obj}')
+
+    filename = r'C:\01_Data_Adithya\Practice_Code\Design_Patterns_Python\design-patterns-python\solid-principles\journal_entries.txt'
+    PersitanceManager.save_entry_to_file(journal=journal_obj, filename=filename)
+    with open(filename) as j_entries:
+        print(f'\nNow printing the journal entries from the file... \n{j_entries.read()}')
